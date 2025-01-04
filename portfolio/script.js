2acb5b845d399b122828829f4c4a6557fd657c40
aaryananjna <137453832+aaryananjna@users.noreply.github.com>
2025-01-04 19:03:48 +0000

    made changes to webpage

---
+++ portfolio/script.js
@@ -0,0 +1,29 @@
+const sideMenu = document.querySelector('#sideMenu');
+const navBar = document.querySelector("nav");
+const navLinks = document.querySelector("nav ul");
+
+function openMenu(){
+    sideMenu.style.transform. = 'translateX(-16rem)';
+}
+
+function closeMenu(){
+    sideMenu.style.transform. = 'translateX(16rem)';
+}
+
+window.addEventListener('scroll', ()=>{
+    if (scrollY > 50){
+        navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50');
+    }else{
+        navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50');
+    }
+})
+
+
+// light mode and dark mode
+
+document.documentElement.classList.toggle(
+    'dark',
+    localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+  )
