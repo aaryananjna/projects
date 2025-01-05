5f0ea9dfa1dd51e77e74c242e6f8ef91bce67848
aaryananjna <137453832+aaryananjna@users.noreply.github.com>
2025-01-05 01:44:24 +0000

    made changes to webpage

--- portfolio/script.js
+++ portfolio/script.js
@@ -12,27 +12,29 @@ function closeMenu(){
 
 window.addEventListener('scroll', ()=>{
     if (scrollY > 50){
-        navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
-        navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50');
+        navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm', 'dark:bg-darkTheme', 'dark:shadow-white/20');
+        navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50', 'dark:border', 'dark:border-white/50', 'dark:bg-transparent');
     }else{
-        navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
-        navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50');
+        navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm', 'dark:bg-darkTheme', 'dark:shadow-white/20');
+        navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50', 'dark:border', 'dark:border-white/50', 'dark:bg-transparent');
     }
 })
 
 
 // light mode and dark mode
 
-document.documentElement.classList.toggle(
-    'dark',
-    localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-  )
-
-  function toggleTheme(){
-    document.documentElement.classList.toggle('dark');
-    if(document.documentElement.classList.contains('dark')){
-        localStorage.theme = 'dark';
+if(localStorage.theme === 'dark' || (!('theme' in localStorage) && window.
+matchMedia('(prefers-color-scheme: dark)').matches)){
+    document.documentElement.classList.add('dark')
     }else{
-        localStorage.theme = 'light';
+        document.documentElement.classList.remove('dark')
     }
-  }
+
+    function toggleTheme(){
+        document.documentElement.classList.toggle('dark');
+        if(document.documentElement.classList.contains('dark')){
+            localStorage.theme = 'dark';
+        }else{
+            localStorage.theme = 'light';
+        }
+    }
